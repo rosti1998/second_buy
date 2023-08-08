4da71e109cdc78dedc1eb7aa3ce1411e928a7163
Rostislav <r.stanislavov@blubito.com>
2023-08-08 10:39:20 +0300

    feat: register models in admin console

--- second_buy/web/admin.py
+++ second_buy/web/admin.py
@@ -1,6 +1,10 @@
 
 from django.contrib import admin
-from .models import Profile, Category, Item
+from .models import Profile, Category, Item, Comment, Inquiry, Discussion
 
 admin.site.register(Profile)
 admin.site.register(Category)
+admin.site.register(Item)
+admin.site.register(Comment)
+admin.site.register(Inquiry)
+admin.site.register(Discussion)
